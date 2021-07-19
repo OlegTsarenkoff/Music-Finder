7a50e31a5e4c7f8601d01e648896761317685c2d
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-07-19 15:47:32 +0300

    add MainTabBarController, Search and Library VC, icons item

---
+++ Music Finder/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Music Finder
+//
+//  Created by Oleg Tsarenkoff on 19.07.21.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
