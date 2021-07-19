61b295f34b4de66e08196732b9b87794aba3b7ea
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-07-19 17:38:08 +0300

    Smart refactoring patterns

---
+++ Music Finder/NetworkService.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkService.swift
+//  Music Finder
+//
+//  Created by Oleg Tsarenkoff on 19.07.21.
+//
+
+import UIKit
+import Alamofire
+
+class NetworkService {
+
+    func fetchTracks(searchText: String, completion: @escaping (SearchResponse?) -> Void) {
+        let url = "https://itunes.apple.com/search"
+        let parameters = ["term":"\(searchText)", "limit":"10", "media":"music"]
+        
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+            if let error = dataResponse.error {
+                print("Error received requestiong data: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let objects = try decoder.decode(SearchResponse.self, from: data)
+                print("Objects:", objects)
+                completion(objects)
+                
+            } catch let jsonError {
+                print("Failed to decode JSON", jsonError)
+                completion(nil)
+            }
+//            let someString = String(data: data, encoding: .utf8)
+//            print(someString ?? "")
+        }
+    }
+}
