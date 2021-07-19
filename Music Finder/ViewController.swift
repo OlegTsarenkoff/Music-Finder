4174ff04cc7cb15d136203da671a22be78868345
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-07-19 15:04:07 +0300

    Initial Commit

---
+++ Music Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Finder
+//
+//  Created by Oleg Tsarenkoff on 19.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
