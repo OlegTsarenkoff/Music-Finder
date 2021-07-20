4fd4a955a3fb6df20375fb242d7d261acd533b15
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-07-20 15:39:42 +0300

    Track detail controller

---
+++ Music Finder/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailView.swift
+//  Music Finder
+//
+//  Created by Oleg Tsarenkoff on 20.07.21.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
