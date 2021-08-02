e106d9810a00e9ee333d819391912d868d8c036a
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-02 11:12:42 +0300

    SwiftUI Gestures and Custom Buttons

--- Music Finder/Library.swift
+++ Music Finder/Library.swift
@@ -10,7 +10,12 @@ import URLImage
 
 struct Library: View {
     
-    var tracks = UserDefaults.standard.savedTracks()
+    @State var tracks = UserDefaults.standard.savedTracks()
+    @State private var showAlert = false
+    @State private var track: SearchViewModel.Cell!
+    
+    var tabBarDelegate: MainTabBarControllerDelegate?
+    
     
     var body: some View {
         NavigationView {
@@ -18,20 +23,23 @@ struct Library: View {
                 GeometryReader { geometry in
                     HStack(spacing: 20) {
                         Button(action: {
-                            print("123")
+                            self.track = self.tracks[0]
+                            self.tabBarDelegate?.maximiseTrackDetailController(viewModel: self.track)
                         }, label: {
                             Image(systemName: "play.fill")
-                                .frame(width: geometry.size.width / 2 - 10, height: 50)
+                                .frame(width: geometry.size.width / 2 - 10,
+                                       height: 50)
                                 .accentColor(Color.init(#colorLiteral(red: 1, green: 0.2156862745, blue: 0.3725490196, alpha: 1)))
                                 .background(Color.init(#colorLiteral(red: 0.9161354899, green: 0.9106894135, blue: 0.9203217626, alpha: 1)))
                                 .cornerRadius(10)
+                            
                         })
-                        
                         Button(action: {
-                            print("123")
+                            self.tracks = UserDefaults.standard.savedTracks()
                         }, label: {
                             Image(systemName: "arrow.2.circlepath")
-                                .frame(width: geometry.size.width / 2 - 10, height: 50)
+                                .frame(width: geometry.size.width / 2 - 10,
+                                       height: 50)
                                 .accentColor(Color.init(#colorLiteral(red: 1, green: 0.2156862745, blue: 0.3725490196, alpha: 1)))
                                 .background(Color.init(#colorLiteral(red: 0.9161354899, green: 0.9106894135, blue: 0.9203217626, alpha: 1)))
                                 .cornerRadius(10)
@@ -39,13 +47,50 @@ struct Library: View {
                     }
                 }.padding().frame(height: 70)
                 Divider().padding(.leading).padding(.trailing)
-                List(tracks) { track in
-                    LibraryCell(cell: track)
+                List {
+                    ForEach(tracks) { track in LibraryCell(cell: track).gesture(
+                        LongPressGesture()
+                            .onEnded { _ in
+                                self.showAlert = true
+                                self.track = track
+                            }
+                            .simultaneously(with:
+                                                TapGesture()
+                                                .onEnded{ _ in
+                                                    self.track = track
+                                                    self.tabBarDelegate?.maximiseTrackDetailController(viewModel: self.track)
+                                                }))
+                    }
+                    .onDelete(perform: delete)
                 }
-            }
+            }.actionSheet(isPresented: $showAlert,
+                          content: {
+                            ActionSheet(title: Text("Delete \(track.artistName) - \(track.trackName)?"),
+                                        buttons: [.destructive(Text("Delete"), action: {
+                                            self.delete(track: self.track)
+                                        }), .cancel()
+                                        ])
+                          })
             .navigationTitle("Library")
         }
-        
+    }
+    
+    func delete(at offsets: IndexSet) {
+        tracks.remove(atOffsets: offsets)
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: tracks, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
+        }
+    }
+    
+    func delete(track: SearchViewModel.Cell) {
+        let index = tracks.firstIndex(of: track)
+        guard let myIndex = index else { return }
+        tracks.remove(at: myIndex)
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: tracks, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
+        }
     }
 }
 
@@ -55,7 +100,6 @@ struct LibraryCell: View {
     
     var body: some View {
         HStack {
-            //URLImage(cell.iconUrlString).resizable().cornerRadius(5).frame(width: 80, height: 50))
             URLImage(URL(string: cell.iconUrlString ?? "")!) { Image in
                 Image
                     .resizable()
--- Music Finder/MainTabBarController.swift
+++ Music Finder/MainTabBarController.swift
@@ -30,7 +30,8 @@ class MainTabBarController: UITabBarController {
         setupTrackDetailView()
         searchVC.tabBarDelegate = self
         
-        let library = Library()
+        var library = Library()
+        library.tabBarDelegate = self
         let hostVC = UIHostingController(rootView: library)
         hostVC.tabBarItem.image = #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon")
         hostVC.tabBarItem.title = "Library"
